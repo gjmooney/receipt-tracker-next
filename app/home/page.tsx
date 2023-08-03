3e89db597cb2366adb8cc8ea7c41298bacbcce3f
Greg <gjmooney@asu.edu>
2023-08-03 18:38:18 +0200

    Add flavor text if no products found

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -10,14 +10,17 @@ const page: FC<pageProps> = async ({}) => {
   const products = await db.product.findMany();
 
   return (
-    <div className="">
+    <div className="flex flex-col gap-4">
       <div>
         <Link href="/add-item" className={buttonVariants()}>
           Add New Item
         </Link>
       </div>
-      <div className="">list of items</div>
-      <ProductList products={products} />
+      {products.length !== 0 ? (
+        <ProductList products={products} />
+      ) : (
+        <div>Add an item to get started</div>
+      )}
     </div>
   );
 };
