dff31c884c71334776ffb817a1459a8e2847716a
Greg <gjmooney@asu.edu>
2023-08-04 12:10:21 +0200

    Move Home to root

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,29 @@
-import Image from "next/image";
+import ProductList from "@/components/ProductList";
+import { buttonVariants } from "@/components/ui/button";
+import { db } from "@/lib/db";
+import Link from "next/link";
+import { FC } from "react";
 
-export default function Home() {
-  return <main>This is the site now. Good job, now log in!</main>;
-}
+interface pageProps {}
+
+const page: FC<pageProps> = async ({}) => {
+  const products = await db.product.findMany();
+
+  return (
+    <main className="flex flex-col gap-4">
+      <div>
+        <p>this is the home page woooo</p>
+        <Link href="/add-item" className={buttonVariants()}>
+          Add New Item
+        </Link>
+      </div>
+      {products.length !== 0 ? (
+        <ProductList products={products} />
+      ) : (
+        <div>Add an item to get started</div>
+      )}
+    </main>
+  );
+};
+
+export default page;
