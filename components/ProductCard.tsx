a5c3d3b8a1f0ed366072bbf54b04aef059c47320
Greg <gjmooney@asu.edu>
2023-08-16 13:55:24 +0200

    Add price per kilo to product card

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -17,6 +17,33 @@ interface ProductCardProps {
   product: Product & { _count: { Price: number } };
 }
 
+const PRODUCE = -1;
+const ERROR = -2;
+
+const findPricePerThousand = (
+  price: number,
+  weight: number,
+  weightUnit: string,
+): number => {
+  // divide price by 100 to convert pennies -> dollars
+  if (weight == PRODUCE) {
+    return price / 100;
+  }
+  const pricePerUnit = price / 100 / weight;
+
+  console.log("weightUnit", weightUnit);
+  switch (weightUnit) {
+    case "KG" || "L":
+      return price / 100;
+    case "G" || "ML":
+      return pricePerUnit * 1000;
+    case "CL":
+      return pricePerUnit * 100;
+    default:
+      return ERROR;
+  }
+};
+
 const ProductCard: FC<ProductCardProps> = async ({ product }) => {
   //console.log("product", product._count.Price);
 
@@ -39,6 +66,12 @@ const ProductCard: FC<ProductCardProps> = async ({ product }) => {
     },
   });
 
+  const pricePerKilo = findPricePerThousand(
+    priceInfo!.price,
+    product.weight,
+    product.weightUnit!,
+  );
+
   return (
     <Card className="flex flex-col justify-evenly bg-slate-100 text-slate-700 md:col-span-6 ">
       <CardHeader className="w-fit">
@@ -47,8 +80,14 @@ const ProductCard: FC<ProductCardProps> = async ({ product }) => {
         </CardTitle>
         <CardDescription className="">
           <span className="lowercase">
-            {product.weight}
-            {product.weightUnit}{" "}
+            {product.weight === -1 ? (
+              <></>
+            ) : (
+              <>
+                {product.weight}
+                {product.weightUnit}{" "}
+              </>
+            )}
             <div className="inline-block capitalize"> {product.category}</div>
           </span>
         </CardDescription>
@@ -75,6 +114,18 @@ const ProductCard: FC<ProductCardProps> = async ({ product }) => {
                 <span className="font-normal text-muted-foreground"> on </span>
                 {format(priceInfo.date, "d MMM yyyy")}
               </span>
+              <br />
+              {pricePerKilo !== ERROR ? (
+                <span>
+                  Price per kilo:{" "}
+                  {new Intl.NumberFormat(undefined, {
+                    style: "currency",
+                    currency: "EUR",
+                  }).format(pricePerKilo)}
+                </span>
+              ) : (
+                <></>
+              )}
             </div>
           ) : (
             <div className="mt-2">No purchases yet</div>
@@ -101,11 +152,11 @@ const ProductCard: FC<ProductCardProps> = async ({ product }) => {
         <div className="flex justify-between gap-4">
           <Link
             href={`/edit-item/${product.id}`}
-            className={cn(buttonVariants(), "basis-1/3")}
+            className={cn(buttonVariants(), "w-fit")}
           >
-            Edit
+            Add Receipt Text
           </Link>
-          <Button className="basis-2/12" variant={"destructive"}>
+          <Button className="basis-1/3" variant={"destructive"}>
             Delete
           </Button>
         </div>
