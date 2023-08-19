94db15853bea50a5b2e1cc61cf137f4c8fa8ba08
Greg <gjmooney@asu.edu>
2023-08-19 19:38:33 +0200

    Update validator

--- lib/validators/addItemForm.ts
+++ lib/validators/addItemForm.ts
@@ -3,8 +3,8 @@ import { z } from "zod";
 //TODO: add custom error messages
 export const AddItemValidator = z.object({
   // required fields
-  type: z.string(),
-  variety: z.string(),
+  type: z.string().min(1),
+  variety: z.string().min(1),
   isProduce: z.boolean().default(false),
 
   // always optional
@@ -14,7 +14,7 @@ export const AddItemValidator = z.object({
   // required for not produce
   weight: z.coerce.number().positive().optional(),
   weightUnit: z.enum(["G", "KG", "ML", "CL", "L", "UNIT"]).optional(),
-  upc: z.string().optional(),
+  upc: z.string().length(13).optional(),
 
   // might keep might delete
   description: z.string().max(150).optional(),
